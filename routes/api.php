fc116b55ded57b0980751475e821e12c7a8a790b
mh3yad <mh3yad@gmail.com>
2025-11-25 11:53:20 +0200

    TASK-3 add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::ApiResource('users', UserController::class);
+Route::ApiResource('tasks', TaskController::class);
