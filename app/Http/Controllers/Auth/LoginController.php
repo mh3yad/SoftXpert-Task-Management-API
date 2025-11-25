a338dc63c985a9d34219fd96394f5bf0363ce4e9
mh3yad <mh3yad@gmail.com>
2025-11-25 16:06:54 +0200

    TASK-4 add breeze auth

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+        $user = $request->user();
+        $token = $user->createToken('name')->plainTextToken;
+        return response()->json([
+            'user' => new UserResource($user),
+            'token' => $token,
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        $request->user()->tokens()->delete();
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
-
-Route::ApiResource('users', UserController::class);
-Route::ApiResource('tasks', TaskController::class);
+require __DIR__ .'/auth.php';
+Route::middleware('auth:sanctum')->group(function () {
+   Route::apiResource('tasks', TaskController::class);
+});
